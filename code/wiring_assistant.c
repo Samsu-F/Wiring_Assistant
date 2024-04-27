97fd6639e97133ffa8b4a29883b78a0b8ca1508f
Samsu-F <92742829+Samsu-F@users.noreply.github.com>
2024-04-27 23:24:18 +0200

    refactor, `implement init_bloom_filters`

--- code/wiring_assistant.c
+++ code/wiring_assistant.c
@@ -5,8 +5,8 @@
 #include <stdint.h>
 #include <assert.h>
 
-#define BLOOM1_SIZE 1019
-#define BLOOM2_SIZE 1021
+#define BLOOM1_SIZE 1019    // 1019 is prime
+#define BLOOM2_SIZE 1021    // 1021 is prime
 #define MAX_M 99            // 0 < m < 100
 #define MAX_S 999999999     // 0 < s < 1000000000
 
@@ -27,20 +27,32 @@ typedef struct graph {
     uint m;     // number of existing cables, m <= 0 <= MAX_M
     uint32_t s; // size (s = length = width)
     uint32_t p1x, p1y, p2x, p2y;    // the points that have to be connected
-    bool bloom1_x[BLOOM1_SIZE];
-    bool bloom1_y[BLOOM1_SIZE];
-    bool bloom2_x[BLOOM2_SIZE];
-    bool bloom2_y[BLOOM2_SIZE];
+    bool bloom1_horizontal[BLOOM1_SIZE];    // bloom filter 1 for horizontal cables
+    bool bloom1_vertical[BLOOM1_SIZE];      // bloom filter 1 for vertical cables
+    bool bloom2_horizontal[BLOOM2_SIZE];    // bloom filter 2 for horizontal cables
+    bool bloom2_vertical[BLOOM2_SIZE];      // bloom filter 2 for vertical cables
     uint m_horizontal;                      // the number of horizontal cables
-    horizontal_cable horizontal_cables[99]; // sorted by their y coordinate
+    horizontal_cable horizontal_cables[MAX_M]; // sorted by their y coordinate
     uint m_vertical;                        // the number of vertical cables
-    vertical_cable vertical_cables[99];     // sorted by their x coordinate
+    vertical_cable vertical_cables[MAX_M];     // sorted by their x coordinate
 } graph;
+
+
+inline uint hash_bloom1(uint32_t key)
+{
+    return key % BLOOM1_SIZE;
+}
+
+inline uint hash_bloom2(uint32_t key)
+{
+    return key % BLOOM2_SIZE;
+}
 
 
 int compare_cables(const void* a, const void* b)
 {
-    vertical_cable* cable_a = (vertical_cable*)a; // TODO: is this fine to use for horizontal cables or is this undefined behavior?
+    // TODO: using for horizontal cables is technically undefined behavior, although it is very unlikely to go wrong
+    vertical_cable* cable_a = (vertical_cable*)a;
     vertical_cable* cable_b = (vertical_cable*)b;
     if(cable_a->x < cable_b->x) {
         return -1;
@@ -56,8 +68,47 @@ int compare_cables(const void* a, const void* b)
 }
 
 
+void insert_cable(graph* const g, const uint x1, const uint y1, const uint x2, const uint y2)
+{
+    if(x1 == x2) {  // vertical cable
+        assert(y1 < y2);
+        g->vertical_cables[g->m_horizontal].x = x1;
+        g->vertical_cables[g->m_horizontal].y1 = y1;
+        g->vertical_cables[g->m_horizontal].y2 = y2;
+        g->m_horizontal++;
+    } else {        // horizontal cable
+        assert(y1 == y2 && x1 < x2);
+        g->horizontal_cables[g->m_vertical].y = y1;
+        g->horizontal_cables[g->m_vertical].x1 = x1;
+        g->horizontal_cables[g->m_vertical].x2 = x2;
+        g->m_vertical++;
+    }
+}
+
+
+void init_bloom_filters(graph* const g)
+{
+    // content of bloom filters is undefined, so clear the bloom filters first
+    memset(g->bloom1_horizontal, false, BLOOM1_SIZE * sizeof(bool));
+    memset(g->bloom2_horizontal, false, BLOOM2_SIZE * sizeof(bool));
+    memset(g->bloom1_vertical, false, BLOOM1_SIZE * sizeof(bool));
+    memset(g->bloom2_vertical, false, BLOOM2_SIZE * sizeof(bool));
+
+    for(uint i = 0; i < g->m_horizontal; i++) {
+        uint32_t key = g->horizontal_cables[i].y;
+        g->bloom1_horizontal[hash_bloom1(key)] = true;
+        g->bloom2_horizontal[hash_bloom2(key)] = true;
+    }
+    for(uint i = 0; i < g->m_vertical; i++) {
+        uint32_t key = g->vertical_cables[i].x;
+        g->bloom1_vertical[hash_bloom1(key)] = true;
+        g->bloom2_vertical[hash_bloom2(key)] = true;
+    }
+}
+
+
 // parse stdin and create the graph struct.
-// returns NULL if there is no next problem instance
+// exits immediately if there is no next problem instance // TODO: is this reasonable?
 graph* parse_instance()
 {
     graph* const g = malloc(sizeof(graph));
@@ -70,43 +121,32 @@ graph* parse_instance()
 
     // read first line. Semantics: M S; Format ^[0-9]{1,2} [0-9]{1,9}$
     scanf("%u %u\n", &(g->m), &(g->s));
-    fprintf(stderr, "DEBUG: g->m = %u, g->s = %u\n", g->m, g->s); ////////////////// debug
+    fprintf(stderr, "\nDEBUG:\tM = %u; S = %u\n", g->m, g->s); // DEBUG
     if(!g->s) { exit(EXIT_SUCCESS); } // the line just parsed marks the end of the input // TODO: is this good style?
 
     // read second line. Semantics: (x_left y_bottom x_right y_bottom)*M; Format [0-9]{1,9} 4M times
     for(uint i = 0; i < g->m; i++) {
         uint x1, y1, x2, y2;
         scanf("%u %u %u %u", &x1, &y1, &x2, &y2);
-        printf("DEBUG: x1=%u y1=%u x2=%u y2=%u\n", x1, y1, x2, y2); ////////////////////////////// debug
-        if(x1 == x2) { // vertical cable
-            assert(y1 < y2);
-            g->vertical_cables[g->m_horizontal].x = x1;
-            g->vertical_cables[g->m_horizontal].y1 = y1;
-            g->vertical_cables[g->m_horizontal].y2 = y2;
-            g->m_horizontal++;
-        } else { // vertical cable
-            assert(y1 == y2 && x1 < x2);
-            g->horizontal_cables[g->m_vertical].y = y1;
-            g->horizontal_cables[g->m_vertical].x1 = x1;
-            g->horizontal_cables[g->m_vertical].x2 = x2;
-            g->m_vertical++;
-        }
+        insert_cable(g, x1, y1, x2, y2);
     }
-    // sort cables
+    // sort cables and init bloom filters
     qsort(g->horizontal_cables, g->m_horizontal, sizeof(horizontal_cable), compare_cables);
     qsort(g->vertical_cables, g->m_vertical, sizeof(vertical_cable), compare_cables);
+    init_bloom_filters(g);
+
 
     // read third line. Semantics: p1_x p1_y p2_x p2_y; Format ^[0-9]{1,9} [0-9]{1,9} [0-9]{1,9} [0-9]{1,9}$
     scanf("%u %u %u %u", &(g->p1x), &(g->p1y), &(g->p2x), &(g->p2y));
 
-    /* DEBUG*/ printf("HORIZONTAL:\n"); for(uint i = 0; i < g->m_horizontal; i++){ printf("\t(%u, %u) (%u, %u)\n", g->horizontal_cables[i].x1, g->horizontal_cables[i].y, g->horizontal_cables[i].x2, g->horizontal_cables[i].y);}printf("VERTICAL:\n");for(uint i = 0; i < g->m_vertical; i++){printf("\t(%u, %u) (%u, %u)\n", g->vertical_cables[i].x, g->vertical_cables[i].y1, g->vertical_cables[i].x, g->vertical_cables[i].y2);}
+    /* DEBUG*/ fprintf(stderr, "HORIZONTAL:\n"); for(uint i = 0; i < g->m_horizontal; i++){ fprintf(stderr, "\t(%u, %u) (%u, %u)\n", g->horizontal_cables[i].x1, g->horizontal_cables[i].y, g->horizontal_cables[i].x2, g->horizontal_cables[i].y);} fprintf(stderr, "VERTICAL:\n");for(uint i = 0; i < g->m_vertical; i++){ fprintf(stderr, "\t(%u, %u) (%u, %u)\n", g->vertical_cables[i].x, g->vertical_cables[i].y1, g->vertical_cables[i].x, g->vertical_cables[i].y2);}
     return g;
 }
 
 
-int main(/*int argc, char* argv[]*/)
+int main()
 {
-    while(1) {
+    while(true) {
         const graph* const g = parse_instance();
         if(!g) { return EXIT_FAILURE; }
     }
