3ae08cdb17ede6e91bb5523f8c796e547c14f9e1
Samsu-F <samsu-f@web.de>
2024-09-13 15:33:54 +0200

    Rename `PathCost` to `PathMetric` for consistent terminology of paper and implementation

--- code/a_star.c
+++ code/a_star.c
@@ -8,19 +8,19 @@
 
 
 
-typedef struct PathCost {
+typedef struct PathMetric {
     uint16_t intersections;
     uint16_t length;
-} PathCost;
+} PathMetric;
 
-#define PQ_KEY_TYPE PathCost
+#define PQ_KEY_TYPE PathMetric
 #define PQ_VAL_TYPE Uint16Point
 #include "pqueue.h"
 
 
 
 // comparison function for the priority queue used in the path search
-static bool cheaper_path(const PathCost a, const PathCost b)
+static bool cheaper_path(const PathMetric a, const PathMetric b)
 {
     uint32_t metric_a = a.length | ((uint32_t)a.intersections << (sizeof(a.length) * CHAR_BIT));
     uint32_t metric_b = b.length | ((uint32_t)b.intersections << (sizeof(b.length) * CHAR_BIT));
@@ -62,13 +62,13 @@ static void free_matrix(void** matrix)
 // Allocate and initialize a scores table for the A* algorithm.
 // May return NULL if allocation failed.
 // Caller is responsible for freeing the table again with free_scores_table [see below].
-static PathCost** new_scores_table(const size_t width, const size_t height, const uint8_t init_byte_value)
+static PathMetric** new_scores_table(const size_t width, const size_t height, const uint8_t init_byte_value)
 {
-    static_assert((sizeof(PathCost**) == sizeof(void*)) && (sizeof(PathCost*) == sizeof(void*)));
-    return (PathCost**)new_matrix(width, height, init_byte_value, sizeof(PathCost));
+    static_assert((sizeof(PathMetric**) == sizeof(void*)) && (sizeof(PathMetric*) == sizeof(void*)));
+    return (PathMetric**)new_matrix(width, height, init_byte_value, sizeof(PathMetric));
 }
 // wrapper function, free a scores table that was allocated by new_scores_table and all of its internal allocations
-static void free_scores_table(PathCost** scores)
+static void free_scores_table(PathMetric** scores)
 {
     free_matrix((void**)scores);
 }
@@ -109,28 +109,30 @@ void free_path_map(bool** path_map)
 
 // Calculate the minimal cost possible for a path between p1 and p2, where the cost of a path is
 // defined as the sum of the node costs of all the nodes in the path, including start and end.
+// Fill the predecessor table iff pred_tbl != NULL, else ignore it.
+// Caller is responsible for ensuring pred_tbl is either NULL or a valid predecessor table.
 static int16_t a_star(const Graph* const g, HeuristicFunc h, Uint16Point** pred_tbl)
 {
     const Uint16Point p1 = g->p1;
     const Uint16Point p2 = g->p2;
 
-    PQueue* openset = pq_new(cheaper_path); // path costs are keys, node ids are values
+    PQueue* openset = pq_new(cheaper_path); // path metrics are keys, node ids are values
     assert(openset != NULL);
-    PathCost path_cost_p1 = {.intersections = g->node_cost[p1.x][p1.y], .length = 0};
-    pq_insert(openset, (KeyValPair) {.key = path_cost_p1, .val = p1});
+    PathMetric path_metric_p1 = {.intersections = g->node_cost[p1.x][p1.y], .length = 0};
+    pq_insert(openset, (KeyValPair) {.key = path_metric_p1, .val = p1});
 
-    PathCost** g_scores = new_scores_table(g->width, g->height, 0xFF);
+    PathMetric** g_scores = new_scores_table(g->width, g->height, 0xFF);
     if(!g_scores) {
         fprintf(stderr, "Allocation for g_scores table failed.\n");
         exit(EXIT_FAILURE);
     }
 
-    g_scores[p1.x][p1.y] = path_cost_p1;
+    g_scores[p1.x][p1.y] = path_metric_p1;
 
     while(!pq_is_empty(openset)) {
         const KeyValPair current = pq_pop(openset);
         const Uint16Point cur_point = current.val;
-        const PathCost cur_g_score = g_scores[cur_point.x][cur_point.y];
+        const PathMetric cur_g_score = g_scores[cur_point.x][cur_point.y];
         if(cur_point.x == p2.x && cur_point.y == p2.y) { // if current point is goal
             pq_free(openset);
             free_scores_table(g_scores);
@@ -156,12 +158,12 @@ static int16_t a_star(const Graph* const g, HeuristicFunc h, Uint16Point** pred_
         // for each neighbor of current
         for(int i = 0; i < neigh_count; i++) {
             const Uint16Point neighbor = neighbors[i];
-            const PathCost tent_g_score = {cur_g_score.intersections +
-                                               g->node_cost[neighbor.x][neighbor.y],
-                                           cur_g_score.length + 1};
+            const PathMetric tent_g_score = {cur_g_score.intersections +
+                                                 g->node_cost[neighbor.x][neighbor.y],
+                                             cur_g_score.length + 1};
             if(cheaper_path(tent_g_score, g_scores[neighbor.x][neighbor.y])) {
                 g_scores[neighbor.x][neighbor.y] = tent_g_score;
-                PathCost neigh_f_score = {tent_g_score.intersections, tent_g_score.length + h(neighbor, p2)};
+                PathMetric neigh_f_score = {tent_g_score.intersections, tent_g_score.length + h(neighbor, p2)};
                 pq_insert(openset, (KeyValPair) {neigh_f_score, neighbor});
                 if(pred_tbl) {
                     pred_tbl[neighbor.x][neighbor.y] = (Uint16Point) {cur_point.x, cur_point.y};
